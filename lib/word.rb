511f97c8a5de1665e9515ed8d96230d1801ff28b
John Michael <john.parsons85210@gmail.com>
2017-09-15 12:11:26 -0700

    create Word Class
    repurposing previous methods

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,37 @@
-class Word{
+class Word
 
-}
+  @@list = []
+
+
+def initialize(word_hash)
+  @name = word_hash["name"]
+  @definition = word_hash["definition"]
+  @id = @@list.length +1
+end
+
+def self.all()
+  @@list
+end
+
+def save()
+  @@list.push(self)
+end
+
+def self.remove(id)
+  @@list.map do |word|
+    if word.id == id
+      word.name = ""
+      word.definition = ""
+    end
+  end
+end
+
+def self.update(id, name, definition)
+  @@list.map do |word|
+    if word.id == id
+      word.name = name
+      word.definition = definition
+    end
+  end
+end
+end
