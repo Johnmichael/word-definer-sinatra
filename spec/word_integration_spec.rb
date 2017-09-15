20aab11d5c63d90232b568ad44e062d0d72965c8
John Michael <john.parsons85210@gmail.com>
2017-09-15 14:04:54 -0700

    finish capybara spec

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,2 +1,30 @@
 require('capybara/rspec')
 require('./app')
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('Create, Update, and Delete', {:type => :feature}) do
+  it('Processes user input and creates the word or phrase') do
+    visit('/')
+    fill_in('name', :with => 'Code Review')
+    fill_in('definition', :with => 'The thing with the objectives')
+    click_button('Add Word')
+    expect(page).to have_content('Code Review')
+  end
+
+  it('Updates the definition') do
+    visit('/1')
+    click_button('Update')
+    fill_in('definition', :with => 'An exercise covering this weeks (and prior) lessons and concepts.')
+    click_button('Confirm')
+    visit('/1')
+    expect(page).to have_content('An exercise covering this weeks (and prior) lessons and concepts.')
+  end
+
+  it('Deletes the user input and returns home') do
+    visit('/1')
+    click_button('Delete')
+    expect(page).to have_no_content('Code Review')
+  end
+end
