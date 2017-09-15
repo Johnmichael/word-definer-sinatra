255005aa53dd8844f72acc78010a969e75b50e2e
John Michael <john.parsons85210@gmail.com>
2017-09-15 12:15:47 -0700

    add boilerplate code
    repurposed code from previous projects

--- app.rb
+++ app.rb
@@ -3,3 +3,49 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
 require('pry')
+
+get('/') do
+  @list = Word.all
+  erb(:input)
+end
+
+post('/') do
+  name = params["name"]
+  definition = params["definition"]
+
+  word_hash = {"name" => name, "definition" => definition}
+
+  word = Word.new(word_hash)
+  word.save
+  @list = Word.all
+  erb(:input)
+end
+
+get('/:id') do
+  @word = Word.search(params[:id])
+  erb(:word_detail)
+end
+
+post('/:id') do
+  @word = Word.search(params[:id])
+  Word.remove(@word.id)
+  redirect '/'
+  erb(:word_detail)
+end
+
+
+get('/update/:id') do
+  @word = Word.search(params[:id])
+  erb(:update)
+end
+
+post('/update/:id') do
+  @word = Word.search(params[:id])
+
+  name = params["name"]
+  definition = params["definition"]
+
+  Word.update(@word.id, name, definition)
+  redirect '/'
+  erb(:update)
+end
